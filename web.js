c830126cc70f259fd9481edbaad44e217a55e77b
tomas-gerry <tomas.gerry.x@gmail.com>
2024-08-13 23:45:47 -0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,25 @@
+console.log("hola dantee jaja")
+
+document.getElementById("boton").onclick = function(){
+    document.getElementById("textoDeArriba").textContent = "GRACIASS EXCLAVOOO!! ajsjajsjaj";
+    document.getElementById("text1").textContent = "sos un boludo";
+}
+
+/*let variable;
+variable = Math.random();
+if (variable >= 0 && variable <= 1/3) {
+    variable="rock";
+}
+
+else if (variable >= 1/3 && variable <=  2/3) {
+    variable = "paper";
+}
+
+else {
+    variable = "sissors";
+}
+
+document.getElementById("ramdomm").textContent = variable ;
+
+document.getElementById("jaja").textContent = "si";
+/** */
